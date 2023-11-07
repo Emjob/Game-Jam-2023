1208f2c10019cf62afbf45fc1130508fa4616b7f
Emjob <99894739+Emjob@users.noreply.github.com>
2023-11-07 21:39:42 +0000

    Dash

--- Assets/Scripts/Character_Movement.cs
+++ Assets/Scripts/Character_Movement.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 
 public class Character_Movement : MonoBehaviour
 {
+    Rigidbody rb;
+
     public float playerSpeed;
     private float horizontalInput;
     private float verticalInput;
 
+    public float dashDuration;
+    public float dashSpeed;
+    private bool isDashing;
+
+    private Vector3 input;
     private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        if(isDashing)
+        {
+            return;
+        }
 
-        Vector3 input = new Vector3 (horizontalInput, 0, verticalInput);
-        transform.Translate(input * Time.deltaTime * playerSpeed);
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        input = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        rb.velocity = new Vector3(input.x * playerSpeed, 0, input.z * playerSpeed);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Dash());
+        }
 
     }
     
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        rb.velocity = new Vector3(input.x * dashSpeed, 0, input.z * dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+    }
 }
