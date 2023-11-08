ad635211bba2d364a6977d46c4d765186785655a
Emjob <99894739+Emjob@users.noreply.github.com>
2023-11-08 13:24:09 +0000

    Loaded

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,58 +6,65 @@ public class Gun : MonoBehaviour
 {
 
     [SerializeField]private GameObject[] Bullet;
+    public string Loaded;
+    private int currentBullet;
     private int Ammo;
     [SerializeField]private Transform Revolver;
 
     void Start()
     {
         Ammo = 1;
+     //   Bullet = GameObject.FindGameObjectsWithTag("Bullet");
     }
 
     void Update()
     {
+
         if(Input.GetMouseButtonDown(0))
         {
             Shoot(Ammo);
         }
+        Loaded = Bullet[currentBullet].name;
     }
     private void Shoot(int Round)
     {
       switch(Ammo)
       {
         case 1:
+        currentBullet = 0;
         Debug.Log("RED");
         Ammo++;
         break;
 
         case 2:
+        currentBullet++;
         Debug.Log("BLUE");
         Ammo++;
         break;
         
         case 3:
+        currentBullet++;
         Debug.Log("GREEN");
         Ammo++;
         break;
         
         case 4:
+        currentBullet++;
         Debug.Log("BROWN");
         Ammo++;
         break;
         
         case 5:
+        currentBullet++;
         Debug.Log("YELLOW");
         Ammo++;
         break;
 
         case 6:
+        currentBullet++;
         Debug.Log("PURPLE");                                                                                                                                                                                                                                                                                                                                               
         Ammo = 1;
         break;
-      }
-
-        
-    }
-    
-
+      } 
+    }  
 }
